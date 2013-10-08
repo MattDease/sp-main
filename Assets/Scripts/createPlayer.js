84b61ed9c741a4d29cda0fde793cbf8569d061db
Desiree Melcer <desiree.walko@gmail.com>
2013-10-08 11:45:31 -0400

    Update Player Gesture and Create Player Scripts

---
+++ Assets/Scripts/createPlayer.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+
+function OnGUI () {
+    // Make a background box
+    GUI.Box (Rect (10,10,100,40), "Loader Menu");
+
+    // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+    if (GUI.Button (Rect (20,40,80,20), "Example")) {
+        loadCharacter("Player One", "Twilight/twilight");
+    }
+}
+
+function loadCharacter(type, url) {
+
+    //Load a Character from the resource folder, set up its player controls
+
+    var instance : GameObject = Instantiate(Resources.Load(url), Vector3(Random.Range(0,100), 0, 0), Quaternion.identity);
+    instance.name = type;
+    instance.AddComponent(BoxCollider);
+    instance.AddComponent(Rigidbody);
+
+    var instanceTransform = instance.GetComponent(Transform);
+
+    var controlsGameObject = new GameObject(type + "_Controls");
+    controlsGameObject.AddComponent('playerGesture');
+    var instanceControls  =  controlsGameObject.GetComponent(playerGesture);
+    instanceControls.projectileObject = instanceTransform;
+
+}
