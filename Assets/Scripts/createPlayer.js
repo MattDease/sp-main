aae602fc8cb054dc082c84c539de5f41e651f34b
Desiree Melcer <desiree.walko@gmail.com>
2013-10-18 11:34:24 -0400

    Player walking and jumping using gestures.

--- Assets/Scripts/createPlayer.js
+++ Assets/Scripts/createPlayer.js
@@ -1,5 +1,7 @@
 ﻿#pragma strict
 
+private var isGui = true;
+
 function Start () {
 
 }
@@ -10,29 +12,41 @@ function Update () {
 
 
 function OnGUI () {
-    // Make a background box
-    GUI.Box (Rect (10,10,100,40), "Loader Menu");
+    // Make a background box (Simple Example GUI), I know its ugly!
+    if(isGui){
+        GUI.Box (Rect (10,10,100,60), "Loader Menu");
 
-    // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-    if (GUI.Button (Rect (20,40,80,20), "Example")) {
-        loadCharacter("Player One", "Twilight/twilight");
+        // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+        if (GUI.Button (Rect (20,40,80,20), "Example")) {
+            loadCharacter("Name", "SampleCharacter",Vector3(-50, 0, 0), Quaternion.identity );
+        }
     }
+
 }
 
-function loadCharacter(type, url) {
+function loadCharacter(modelName, url, position, rotation) {
 
     //Load a Character from the resource folder, set up its player controls
 
-    var instance : GameObject = Instantiate(Resources.Load(url), Vector3(Random.Range(0,100), 0, 0), Quaternion.identity);
-    instance.name = type;
+    var instance : GameObject = Instantiate(Resources.Load(url), position, rotation);
+    //Add correpsonding components to the model
+
     instance.AddComponent(BoxCollider);
     instance.AddComponent(Rigidbody);
 
+    //Set up anything specific about the model
+    instance.name = modelName;
+    instance.transform.localScale = Vector3(4, 4, 4);
+    instance.GetComponent(BoxCollider).size = Vector3(5, 0.3, 5);
+
+
     var instanceTransform = instance.GetComponent(Transform);
 
-    var controlsGameObject = new GameObject(type + "_Controls");
+    var controlsGameObject = new GameObject(modelName + "_Controls");
     controlsGameObject.AddComponent('playerGesture');
     var instanceControls  =  controlsGameObject.GetComponent(playerGesture);
-    instanceControls.projectileObject = instanceTransform;
+    instanceControls.model = instanceTransform;
+
+    isGui = false;
 
 }
--- Assets/Scripts/playerGesture.js
+++ Assets/Scripts/playerGesture.js
@@ -1,23 +1,80 @@
 #pragma strict
 
-public var projectileObject:Transform;
+public var model:Transform;
 private var body:Rigidbody;
 private var labelTimer:float=-1;
 
+private var isJumping : boolean = false;
+private var isCrouching : boolean = false;
+private var isRunning : boolean = false;
+private var isTossingLeft : boolean = false;
+private var isTossingRight : boolean = false;
+private var isWalking : boolean = false;
+
+private var WALK_SPEED : Vector3 = Vector3 (1, 0, 0);
+private var RUN_SPPED : Vector3 = Vector3 (4, 0, 0);
+
+
+private var MAX_HEIGHT : int = 0.5f;
+private var JUMP_SPEED : int = 20;
+
 function Start(){
-	body=projectileObject.gameObject.GetComponent(Rigidbody);
+	body=model.gameObject.GetComponent(Rigidbody);
+    body.freezeRotation = true;
 }
 
 function OnEnable(){
 	Gesture.onSwipeE += OnSwipe;
 	Gesture.onLongTapE += OnLongTap;
 	Gesture.onTouchE += OnTouch;
+	Gesture.onTouchUpE += OnRelease;
+	Gesture.onMouse1E += OnTouch;
+	Gesture.onMouse1UpE += OnRelease;
+
 }
 
 function OnDisable(){
 	Gesture.onSwipeE -= OnSwipe;
 	Gesture.onLongTapE -= OnLongTap;
 	Gesture.onTouchE -= OnTouch;
+	Gesture.onTouchUpE -= OnRelease;
+	Gesture.onMouse1E -= OnTouch;
+	Gesture.onMouse1UpE -= OnRelease;
+}
+
+function Update() {
+
+	/*To make a jump, we add a force in the y direction.
+	When he reaches a max jump height - remove added force and set jumping to false.*/
+	if(isJumping) {
+		body.AddForce(0, JUMP_SPEED, 0);
+
+		if(model.transform.position.y > MAX_HEIGHT) {
+			body.AddForce(0,0,0);
+			body.velocity = Vector3(0,0,0);
+			isJumping = false;
+		}
+	}
+
+	if(isCrouching){
+		//Add actions here
+	}
+
+	if(isTossingLeft){
+		//Add actions here
+	}
+
+	if(isTossingRight) {
+		//Add actions here
+	}
+
+
+	if(isWalking) {
+	  	Walk();
+	} else {
+	 Run();
+	}
+
 }
 
 function OnSwipe(sw:SwipeInfo){
@@ -39,20 +96,19 @@ function OnSwipe(sw:SwipeInfo){
 
 	//Figure out what direction we are swiping
 	if(sw.direction.x > 0  && sw.angle > 45 && sw.angle < 135 ) {
-		Debug.Log("Swipe Up");
+		isJumping = true;
 	}
 
 	if(sw.direction.x < 0  && sw.angle > 235 && sw.angle < 315 ) {
-		Debug.Log("Swipe Down");
+		isCrouching = true;
 	}
 
 	if(sw.direction.y > 0  && ((sw.angle > 0 && sw.angle < 45) || (sw.angle > 315 && sw.angle < 360)) )  {
-		Debug.Log("Swipe Right");
+		isTossingLeft = true;
 	}
 
-
 	if(sw.direction.y < 0  && sw.angle > 135 && sw.angle < 235 ) {
-		Debug.Log("Swipe Left");
+		isTossingRight = true;
 	}
 }
 
@@ -62,5 +118,17 @@ function OnLongTap(tap:Tap){
 }
 
 function OnTouch(pos:Vector2){
-	//Debug.Log("OnTouch - gives x and y coords");
+	isWalking = true;
+}
+function OnRelease(pos:Vector2){
+	isWalking = false;
+}
+
+function Walk() {
+	body.MovePosition(body.position + WALK_SPEED * Time.deltaTime);
+}
+
+function Run() {
+		body.MovePosition(body.position + RUN_SPPED * Time.deltaTime);
+
 }
