c0823c4bebe965c70ba4220ee0f890bf2bae26b0
Desiree Melcer <desiree.walko@gmail.com>
2013-10-23 10:56:46 -0400

    Parent and Unparenting of egg.

--- Assets/Scripts/createPlayer.js
+++ Assets/Scripts/createPlayer.js
@@ -14,12 +14,17 @@ function Update () {
 function OnGUI () {
     // Make a background box (Simple Example GUI), I know its ugly!
     if(isGui){
-        GUI.Box (Rect (10,10,100,60), "Loader Menu");
+        GUI.Box (Rect (10,10,100,90), "Let's load things");
 
         // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-        if (GUI.Button (Rect (20,40,80,20), "Example")) {
+        if (GUI.Button (Rect (20,40,80,20), "Character")) {
             loadCharacter("Name", "SampleCharacter",Vector3(-50, 0, 0), Quaternion.identity );
         }
+
+        if (GUI.Button (Rect (20,70,80,20), "Egg")) {
+            createEgg();
+        }
+
     }
 
 }
@@ -29,8 +34,8 @@ function loadCharacter(modelName, url, position, rotation) {
     //Load a Character from the resource folder, set up its player controls
 
     var instance : GameObject = Instantiate(Resources.Load(url), position, rotation);
-    //Add correpsonding components to the model
 
+    //Add correpsonding components to the model
     instance.AddComponent(BoxCollider);
     instance.AddComponent(Rigidbody);
 
@@ -38,7 +43,6 @@ function loadCharacter(modelName, url, position, rotation) {
     instance.name = modelName;
     instance.transform.localScale = Vector3(4, 4, 4);
     instance.GetComponent(BoxCollider).size = Vector3(5, 0.3, 5);
-
 
     var instanceTransform = instance.GetComponent(Transform);
 
@@ -48,5 +52,17 @@ function loadCharacter(modelName, url, position, rotation) {
     instanceControls.model = instanceTransform;
 
     isGui = false;
+
+}
+
+function createEgg() {
+    var position : Vector3 = Vector3(0,0,0);
+    var rotation  = Quaternion(0,0,0,0);
+    var instance : GameObject = Instantiate(Resources.Load("egg"), position, rotation);
+    instance.AddComponent(BoxCollider);
+    instance.AddComponent(Rigidbody);
+    instance.transform.localScale = Vector3(0.5, 0.5, 0.5);
+    instance.transform.Rotate(0, 0, 90);
+    instance.name = "egg";
 
 }
--- Assets/Scripts/playerGesture.js
+++ Assets/Scripts/playerGesture.js
@@ -2,7 +2,10 @@
 
 public var model:Transform;
 private var body:Rigidbody;
-private var labelTimer:float=-1;
+private var eggBody:Rigidbody;
+
+private var egg: GameObject;
+private var player: GameObject;
 
 private var isJumping : boolean = false;
 private var isCrouching : boolean = false;
@@ -10,15 +13,20 @@ private var isRunning : boolean = false;
 private var isTossingLeft : boolean = false;
 private var isTossingRight : boolean = false;
 private var isWalking : boolean = false;
+private var isCatchingEgg : boolean = false;
+private var hasEgg : boolean = false;
 
+//Variables that determine speed and height.
 private var WALK_SPEED : Vector3 = Vector3 (1, 0, 0);
-private var RUN_SPPED : Vector3 = Vector3 (4, 0, 0);
-
-
+private var RUN_SPEED : Vector3 = Vector3 (4, 0, 0);
 private var MAX_HEIGHT : int = 0.5f;
 private var JUMP_SPEED : int = 20;
 
 function Start(){
+    player = model.gameObject;
+    egg = GameObject.Find("egg");
+    eggBody = egg.GetComponent(Rigidbody);
+    eggBody.freezeRotation = true;
 	body=model.gameObject.GetComponent(Rigidbody);
     body.freezeRotation = true;
 }
@@ -30,7 +38,6 @@ function OnEnable(){
 	Gesture.onTouchUpE += OnRelease;
 	Gesture.onMouse1E += OnTouch;
 	Gesture.onMouse1UpE += OnRelease;
-
 }
 
 function OnDisable(){
@@ -46,7 +53,7 @@ function Update() {
 
 	/*To make a jump, we add a force in the y direction.
 	When he reaches a max jump height - remove added force and set jumping to false.*/
-	if(isJumping) {
+	if(isJumping && !hasEgg) {
 		body.AddForce(0, JUMP_SPEED, 0);
 
 		if(model.transform.position.y > MAX_HEIGHT) {
@@ -60,40 +67,30 @@ function Update() {
 		//Add actions here
 	}
 
-	if(isTossingLeft){
-		//Add actions here
-	}
-
-	if(isTossingRight) {
-		//Add actions here
-	}
-
-
 	if(isWalking) {
 	  	Walk();
 	} else {
 	 Run();
 	}
 
+    if(hasEgg){
+        Parent(player, egg);
+    }
+
 }
 
+
+/* Swipe Info Contains
+	Index of Finger = sw.index;
+	Angle of Swipe Vector = sw.angle;
+	Direction = sw.direction;
+	Duration = sw.duration;
+	Speed = sw.speed;
+	Start Point = sw.startPoint;
+	End Point = sw.endPoint;
+*/
+
 function OnSwipe(sw:SwipeInfo){
-	//show the swipe info
-	var labelText:String="Swipe Detected, ";
-	if(sw.isMouse) labelText+="mouse "+sw.index.ToString()+ " ";
-	else labelText+="finger "+sw.index.ToString()+" ";
-
-	labelText+="Angle: " + sw.angle.ToString() + " ";
-	labelText+="Direction X: " + sw.direction.x.ToString() + " ";
-	labelText+="Direction Y: " + sw.direction.y.ToString() + " ";
-	labelText+="Duration: " + sw.duration.ToString() + " ";
-	labelText+="Speed: " + sw.speed.ToString() + " ";
-	labelText+="Start Pos X: " + sw.startPoint.x.ToString() + " ";
-	labelText+="Start Pos Y: " + sw.startPoint.y.ToString() + " ";
-	labelText+="End Pos X: " + sw.endPoint.x.ToString() + " ";
-	labelText+="End Pos Y: " + sw.endPoint.y.ToString();
-	//Debug.Log(labelText);
-
 	//Figure out what direction we are swiping
 	if(sw.direction.x > 0  && sw.angle > 45 && sw.angle < 135 ) {
 		isJumping = true;
@@ -105,16 +102,20 @@ function OnSwipe(sw:SwipeInfo){
 
 	if(sw.direction.y > 0  && ((sw.angle > 0 && sw.angle < 45) || (sw.angle > 315 && sw.angle < 360)) )  {
 		isTossingLeft = true;
+        Toss(sw.direction, sw.angle);
 	}
 
 	if(sw.direction.y < 0  && sw.angle > 135 && sw.angle < 235 ) {
 		isTossingRight = true;
+        Toss(sw.direction, sw.angle);
 	}
 }
 
 //called when a long tap event is ended
 function OnLongTap(tap:Tap){
 	Debug.Log("On Long Tap");
+    //Just for temp
+    hasEgg = true;
 }
 
 function OnTouch(pos:Vector2){
@@ -129,6 +130,34 @@ function Walk() {
 }
 
 function Run() {
-		body.MovePosition(body.position + RUN_SPPED * Time.deltaTime);
+	body.MovePosition(body.position + RUN_SPEED * Time.deltaTime);
 
+}
+
+function Toss(direction:Vector3, angle){
+    if(hasEgg){
+        hasEgg = false;
+        Unparent(player, egg);
+        eggBody.AddForce(direction);
+    }
+}
+
+function Parent (parentObj:GameObject, childObj:GameObject){
+      Debug.Log("Parent");
+
+      childObj.transform.position = parentObj.transform.position;
+      childObj.transform.position += Vector3(2, 3, 0);
+      childObj.transform.parent = parentObj.transform;
+}
+
+function Unparent(parentObj:GameObject, childObj:GameObject){
+
+    Debug.Log("Unparent");
+    var tempPos = childObj.transform.position;
+    if(parentObj.transform.childCount > 0) {
+        var toRemove = parentObj.transform.Find("egg");
+        toRemove.transform.parent = null;
+        toRemove.transform.position = tempPos;
+    }
+      //parentObj.transform.DetachChildren();
 }
