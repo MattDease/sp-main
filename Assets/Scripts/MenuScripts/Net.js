d824cf83916304f82e688954d32c0c5e273cb2e7
Matt Dease <mattdease+dev@gmail.com>
2013-11-13 10:52:34 -0500

    Add connection testing.

--- Assets/Scripts/MenuScripts/Net.js
+++ Assets/Scripts/MenuScripts/Net.js
@@ -12,6 +12,14 @@ public var hostData : HostData[];
 private var lastHostListRequest : float = 0;
 private var onConnection : Function;
 
+private var natCapable : ConnectionTesterStatus = ConnectionTesterStatus.Undetermined;
+private var filterNATHosts : boolean = false;
+private var probingPublicIP : boolean = false;
+private var doneTestingNAT : boolean = false;
+private var useNat : boolean = false;
+private var timer : float = 0.0;
+private var connTestMessage : String = "Undetermined NAT capabilities";
+
 function Awake () {
     // TODO - enable once hostname has propagated to DNS servers
     // var hostInfo:IPHostEntry  = Dns.GetHostEntry(masterServerHostname);
@@ -28,11 +36,14 @@ function Awake () {
     Network.natFacilitatorPort = 50005;
     Network.connectionTesterIP = "172.19.12.112";
     Network.connectionTesterPort = 10737;
+
+    // Start connection test
+    natCapable = Network.TestConnection();
 }
 
 function Start () {
     MasterServer.ClearHostList();
-    MasterServer.RequestHostList (gameId);
+    MasterServer.RequestHostList(gameId);
 
     yield WaitForSeconds(0.5);
     var tries : int=0;
@@ -44,6 +55,13 @@ function Start () {
         }
         yield WaitForSeconds(0.5);
         tries++;
+    }
+}
+
+function Update() {
+    // If network test is undetermined, keep running
+    if (!doneTestingNAT) {
+        TestConnection();
     }
 }
 
@@ -85,4 +103,98 @@ function FetchHostList(manual : boolean){
 function OnConnectedToServer(){
     onConnection();
     onConnection = null;
+}
+
+function TestConnection() {
+    natCapable = Network.TestConnection();
+
+    switch (natCapable) {
+        case ConnectionTesterStatus.Error:
+            connTestMessage = "Problem determining NAT capabilities";
+            doneTestingNAT = true;
+            break;
+
+        case ConnectionTesterStatus.Undetermined:
+            connTestMessage = "Undetermined NAT capabilities";
+            doneTestingNAT = false;
+            break;
+
+        case ConnectionTesterStatus.PublicIPIsConnectable:
+            connTestMessage = "Directly connectable public IP address.";
+
+            doneTestingNAT = true;
+            useNat = false;
+            break;
+
+        // This case is a bit special as we now need to check if we can
+        // cicrumvent the blocking by using NAT punchthrough
+        case ConnectionTesterStatus.PublicIPPortBlocked:
+            connTestMessage = "Non-connectble public IP address (port " + gamePort +" blocked), running a server is impossible.";
+            useNat = true;
+
+            // If no NAT punchthrough test has been performed on this public IP, force a test
+            if (!probingPublicIP)
+            {
+                Debug.Log("Testing if firewall can be circumnvented");
+                natCapable = Network.TestConnectionNAT();
+                probingPublicIP = true;
+                timer = Time.time + 10;
+            }
+            // NAT punchthrough test was performed but we still get blocked
+            else if (Time.time > timer)
+            {
+                probingPublicIP = false;        // reset
+
+                doneTestingNAT = true;
+                useNat = true;
+            }
+            break;
+
+        case ConnectionTesterStatus.PublicIPNoServerStarted:
+            connTestMessage = "Public IP address but server not initialized, " +
+                              "it must be started to check server accessibility. " +
+                              "Restart connection test when ready.";
+            break;
+
+        case ConnectionTesterStatus.LimitedNATPunchthroughPortRestricted:
+        case ConnectionTesterStatus.LimitedNATPunchthroughSymmetric:
+            connTestMessage = "Limited NAT punchthrough capabilities. Cannot "+
+                              "connect to all types of NAT servers. Running a server "+
+                              "is ill advised as not everyone can connect.";
+            useNat = true;
+            doneTestingNAT = true;
+            break;
+
+        case ConnectionTesterStatus.NATpunchthroughAddressRestrictedCone:
+        case ConnectionTesterStatus.NATpunchthroughFullCone:
+            connTestMessage = "NAT punchthrough capable. Can connect to all "+
+                              "servers and receive connections from all clients. Enabling "+
+                              "NAT punchthrough functionality.";
+            useNat = true;
+            doneTestingNAT = true;
+            break;
+
+        default:
+            connTestMessage = "Error in test routine, got " + natCapable;
+    }
+
+    if(doneTestingNAT){
+        Debug.Log("ConnTester:"+connTestMessage);
+        Debug.Log("ConnTester: NAT Capable? - " + natCapable);
+        Debug.Log("ConnTester: Circmventing Firewall? - " + probingPublicIP);
+        Debug.Log("ConnTester: Connection Test Complete");
+    }
+}
+
+function CanConnect(type1: ConnectionTesterStatus, type2: ConnectionTesterStatus) : boolean{
+    if (type1 == ConnectionTesterStatus.LimitedNATPunchthroughPortRestricted &&
+        type2 == ConnectionTesterStatus.LimitedNATPunchthroughSymmetric)
+        return false;
+    else if (type1 == ConnectionTesterStatus.LimitedNATPunchthroughSymmetric &&
+        type2 == ConnectionTesterStatus.LimitedNATPunchthroughPortRestricted)
+        return false;
+    else if (type1 == ConnectionTesterStatus.LimitedNATPunchthroughSymmetric &&
+        type2 == ConnectionTesterStatus.LimitedNATPunchthroughSymmetric)
+        return false;
+    return true;
 }
