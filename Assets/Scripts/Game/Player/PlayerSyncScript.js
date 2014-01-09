e8be83d770e867e990dbf780f4991d4154c76ec5
Matt Dease <mattdease+dev@gmail.com>
2014-01-08 23:56:08 -0500

    Add script to only syncronize x and y positions of the player.

---
+++ Assets/Scripts/Game/Player/PlayerSyncScript.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function OnSerializeNetworkView(stream : BitStream, info : NetworkMessageInfo) {
+    var posX : float = 0;
+    var posY : float = 0;
+    if (stream.isWriting) {
+        posX = gameObject.transform.position.x;
+        posY = gameObject.transform.position.y;
+        stream.Serialize(posX);
+        stream.Serialize(posY);
+    }
+    else {
+        stream.Serialize(posX);
+        stream.Serialize(posY);
+        gameObject.transform.position.x = posX;
+        gameObject.transform.position.y = posY;
+    }
+}
