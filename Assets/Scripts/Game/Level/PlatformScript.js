4bfa8d7f8669edcb3db279e52947161cd6335af8
Matt Dease <mattdease+dev@gmail.com>
2014-01-31 09:59:00 -0500

    Add method to define drag limits to platforms.
    Limit values are relative the the initial position of the platform.

--- Assets/Scripts/Game/Level/PlatformScript.js
+++ Assets/Scripts/Game/Level/PlatformScript.js
@@ -1,11 +1,39 @@
 ﻿#pragma strict
 
+public var min : Vector2;
+public var max : Vector2;
+
+private var initialPosition : Vector2;
+
+function OnNetworkInstantiate (info : NetworkMessageInfo) {
+    initialPosition = gameObject.transform.position;
+}
+
 function notifyPosition(position : Vector3){
-    if(Network.isServer){
-        setPosition(position);
+    var diff : Vector2 = position - initialPosition;
+    if(gameObject.CompareTag("moveableX")){
+        if(diff.x < min.x){
+            position.x = initialPosition.x + min.x;
+        }
+        else if(diff.x > max.x){
+            position.x = initialPosition.x + max.x;
+        }
     }
-    else{
-        networkView.RPC("setPosition", RPCMode.Server, position);
+    else if(gameObject.CompareTag("moveableY")){
+        if(diff.y < min.y){
+            position.y = initialPosition.y + min.y;
+        }
+        else if(diff.y > max.y){
+            position.y = initialPosition.y + max.y;
+        }
+    }
+    if(position != gameObject.transform.position){
+        if(Network.isServer){
+            setPosition(position);
+        }
+        else{
+            networkView.RPC("setPosition", RPCMode.Server, position);
+        }
     }
 }
 
