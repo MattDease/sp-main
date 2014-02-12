fc70640fe5e001447c9a493ff6aa297f69048672
Matt Dease <mattdease+dev@gmail.com>
2014-02-12 13:43:40 -0500

    Toggle visibility of tutorial signs based on player role.

--- Assets/Scripts/Game/Level/LevelScript.js
+++ Assets/Scripts/Game/Level/LevelScript.js
@@ -1,6 +1,13 @@
 ﻿#pragma strict
 
 function OnNetworkInstantiate (info : NetworkMessageInfo) {
+    var player : Player = GameObject.Find("/GameManager").GetComponent(PlayerScript).getSelf();
+    if(player.GetType() == Commander){
+        Util.Toggle(gameObject.transform.Find("debug_platform3/tut_commander").gameObject, true);
+    }
+    else{
+        Util.Toggle(gameObject.transform.Find("debug_platform3/tut_runner").gameObject, true);
+    }
     if(Network.isServer){
         GameObject.Find("GameScripts").GetComponent(LevelManager).onAddSegment(gameObject);
     }
