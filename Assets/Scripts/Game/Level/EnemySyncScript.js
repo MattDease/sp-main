9ee55dad92000e8b66368032fe719e14b593b835
Matt Dease <mattdease+dev@gmail.com>
2014-03-04 12:48:57 -0500

    Enemy network interpolation.

---
+++ Assets/Scripts/Game/Level/EnemySyncScript.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+private var lastSynchronizationTime : float = 0;
+private var syncDelay : float = 0;
+private var syncTime : float = 0;
+private var syncStartPosition : Vector3 = Vector3.zero;
+private var syncEndPosition : Vector3 = Vector3.zero;
+
+function OnSerializeNetworkView(stream : BitStream, info : NetworkMessageInfo) {
+    var position : Vector3 = Vector3.zero;
+    if (stream.isWriting) {
+        position = transform.position;
+
+        stream.Serialize(position);
+    }
+    else {
+        stream.Serialize(position);
+
+        syncTime = 0;
+        syncDelay = Time.time - lastSynchronizationTime;
+        lastSynchronizationTime = Time.time;
+
+        syncStartPosition = transform.position;
+        syncEndPosition = position;
+    }
+}
+
+function Update(){
+    if (!networkView.isMine){
+        syncTime += Time.deltaTime;
+        transform.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
+    }
+}
