ed534b83e4da6856bf6515dd07945e0a2b2f2ce2
Matt Dease <mattdease+dev@gmail.com>
2014-03-04 10:48:14 -0500

    Commander network interpolation.

--- Assets/Scripts/Game/Player/CommanderSyncScript.js
+++ Assets/Scripts/Game/Player/CommanderSyncScript.js
@@ -1,18 +1,37 @@
 ﻿#pragma strict
 
+private var lastSynchronizationTime : float = 0;
+private var syncDelay : float = 0;
+private var syncTime : float = 0;
+private var syncStartPosition : Vector3 = Vector3.zero;
+private var syncEndPosition : Vector3 = Vector3.zero;
+
 function OnSerializeNetworkView(stream : BitStream, info : NetworkMessageInfo) {
     var posX : float = 0;
     var posY : float = 0;
     if (stream.isWriting) {
-        posX = gameObject.transform.position.x;
-        posY = gameObject.transform.position.y;
+        posX = transform.position.x;
+        posY = transform.position.y;
+
         stream.Serialize(posX);
         stream.Serialize(posY);
     }
     else {
         stream.Serialize(posX);
         stream.Serialize(posY);
-        gameObject.transform.position.x = posX;
-        gameObject.transform.position.y = posY;
+
+        syncTime = 0;
+        syncDelay = Time.time - lastSynchronizationTime;
+        lastSynchronizationTime = Time.time;
+
+        syncStartPosition = transform.position;
+        syncEndPosition = Vector3(posX, posY, transform.position.z);
+    }
+}
+
+function Update(){
+    if (!networkView.isMine){
+        syncTime += Time.deltaTime;
+        transform.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
     }
 }
