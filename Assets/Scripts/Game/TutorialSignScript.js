21e75700f3a377656802fce3364dacc7884adc8c
Desiree Melcer <desiree.walko@gmail.com>
2014-03-08 14:46:44 -0500

    Start of Tutorial Signs

---
+++ Assets/Scripts/Game/TutorialSignScript.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+private var anim: Animator;
+private var currentState : AnimatorStateInfo;
+
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function FixedUpdate() {
+    anim = GetComponent(Animator);
+    currentState = anim.GetCurrentAnimatorStateInfo(0);
+
+    if(Input.GetKey('m')){
+        anim.SetBool("Shown", true);
+    }
+    if(Input.GetKey('n')){
+        anim.SetBool("Shown", false);
+    }
+}
