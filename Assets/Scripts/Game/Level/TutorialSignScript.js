e96d67970147e780397153d8f922a866f24dae42
Matt Dease <mattdease+dev@gmail.com>
2014-04-08 10:19:05 -0400

    Closes #135 - Hide non-applicable signs immediately when egg possession changes.

--- Assets/Scripts/Game/Level/TutorialSignScript.js
+++ Assets/Scripts/Game/Level/TutorialSignScript.js
@@ -1,5 +1,7 @@
 ﻿#pragma strict
 
+// Needs refactoring.
+
 private var anim: Animator;
 private var currentState : AnimatorStateInfo;
 private var game : Game;
@@ -9,6 +11,7 @@ private var isAnimating : boolean = false;
 private var distance : double = 0.0;
 private var egg : GameObject;
 private var eggScript : EggScript;
+private var prevHasEgg : boolean = false;
 private var hasEgg : boolean = false;
 private var forEgg : boolean = false;
 
@@ -27,6 +30,7 @@ function Update() {
     currentState = anim.GetCurrentAnimatorStateInfo(0);
 
     if(!player.getSignWith(sign.name)) {
+        prevHasEgg = hasEgg;
         forEgg = false;
         if(Config.USE_EGG){
             egg = player.getTeam().getEgg();
@@ -40,6 +44,25 @@ function Update() {
 
         if(sign.name.Contains("2") || sign.name.Contains("4")) {
             forEgg = true;
+        }
+
+        if(prevHasEgg != hasEgg){
+            if(hasEgg){
+                if(sign.name.Contains("0") || sign.name.Contains("3")) {
+                    Util.Toggle(gameObject, false);
+                }
+                if(sign.name.Contains("4")) {
+                    Util.Toggle(gameObject, true);
+                }
+            }
+            else{
+                if(sign.name.Contains("4")) {
+                    Util.Toggle(gameObject, false);
+                }
+                if(sign.name.Contains("0") || sign.name.Contains("3")) {
+                    Util.Toggle(gameObject, true);
+                }
+            }
         }
 
         distance = Vector3.Distance(sign.transform.position, player.gameObject.transform.position);
@@ -56,6 +79,7 @@ function Update() {
         if(isAnimating && distance > Config.TUTORIAL_SIGN_DISTANCE) {
             Destroy(sign);
         }
+        prevHasEgg = hasEgg;
     }
     else {
         Destroy(sign);
